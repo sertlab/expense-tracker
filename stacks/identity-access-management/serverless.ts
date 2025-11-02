8f6801c22d2c21ac0674d7970a8a85e34df5b5e4
Fanis Strezos <strezosfanis@gmail.com>
2025-11-02 17:02:05 +0000

    fix ci cd

--- stacks/identity-access-management/serverless.ts
+++ stacks/identity-access-management/serverless.ts
@@ -4,11 +4,24 @@ import cognitoResources from './resources/cognito';
 const config: AWS = {
   service: 'expense-tracker-iam',
   frameworkVersion: '3',
+  plugins: ['serverless-esbuild'],
   provider: {
     name: 'aws',
     runtime: 'nodejs20.x',
     region: 'eu-west-2',
     stage: "${opt:stage, 'dev'}",
+  },
+
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['@aws-sdk/*'],
+      target: 'node20',
+      platform: 'node',
+      concurrency: 10,
+    },
   },
 
   resources: {
